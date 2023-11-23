c907dbf11fb6c5dbd3cf473dbe0e0f94a3103b71
SeolHuiGwan9478 <shg5977@naver.com>
2023-11-24 00:18:02 +0900

    first commit

---
+++ src/main/java/com/likelion/hufsting/HufstingApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.hufsting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HufstingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HufstingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/hufsting/HufstingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.hufsting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HufstingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
