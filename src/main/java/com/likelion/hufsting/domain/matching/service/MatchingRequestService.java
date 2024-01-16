261788e7db75bf71a2887dd58041f2fee94887aa
SeolHuiGwan9478 <shg5977@naver.com>
2024-01-16 17:05:14 +0900

    Feat: 매칭 요청 생성 기능 구현 - 매칭 신청을 받기 위한 API를 생성하기 위해 MatchingRequestController 작성함 -> /api/v1/matchingrequests - 사용자의 요청 DTO로는 매칭글 ID와 대표 신청자 ID 그리고 참가자 ID를 받음. - 매칭 신청 엔티티는 대표자 ID를 기준으로 생성함. - 매칭 신청시 함께 참가하는 참가자들의 ID를 통해 MatchingParticipant 객체를 생성함.

--- src/main/java/com/likelion/hufsting/domain/matching/service/MatchingRequestService.java
+++ src/main/java/com/likelion/hufsting/domain/matching/service/MatchingRequestService.java
@@ -1,7 +1,6 @@
 package com.likelion.hufsting.domain.matching.service;
 
-import com.likelion.hufsting.domain.matching.domain.MatchingPost;
-import com.likelion.hufsting.domain.matching.domain.MatchingRequest;
+import com.likelion.hufsting.domain.matching.domain.*;
 import com.likelion.hufsting.domain.matching.dto.CreateMatchingReqData;
 import com.likelion.hufsting.domain.matching.repository.MatchingPostRepository;
 import com.likelion.hufsting.domain.matching.repository.MatchingRequestRepository;
@@ -24,16 +23,25 @@ public class MatchingRequestService {
     // 매칭 신청 생성
     @Transactional
     public void createMatchingRequests(CreateMatchingReqData dto){
+        Member representative = new Member(); // 임시 대표 신청자
         MatchingPost matchingPost = matchingPostRepository.findById(dto.getMatchingPostId());
+        MatchingRequest newMatchingRequest = MatchingRequest.builder()
+                .matchingPost(matchingPost)
+                .representative(representative)
+                .matchingAcceptance(MatchingAcceptance.WAITING)
+                .build();
         // Member 리스트 가져오기
-        for(Long participantId : dto.getParticipantIds()){
-            Member participant = new Member(); // 임시 사용자
-            MatchingRequest newMatchingRequest = MatchingRequest.builder()
-                    .matchingPost(matchingPost)
-                    .participant(participant)
-                    .build();
-            // 영속화(persist)
-            matchingRequestRepository.save(newMatchingRequest);
+        newMatchingRequest.addParticipant(createMatchingParticipantsById(newMatchingRequest, dto.getParticipantIds()));
+        matchingRequestRepository.save(newMatchingRequest);
+    }
+
+    // create MatchingRequests List By MemberId
+    public static List<MatchingParticipant> createMatchingParticipantsById(MatchingRequest matchingRequest,List<Long> participantIds){
+        List<MatchingParticipant> matchingParticipants = new ArrayList<>();
+        for(Long participantId : participantIds){
+            Member findHost = new Member(); // 임시 사용자 생성
+            matchingParticipants.add(new MatchingParticipant(matchingRequest, findHost));
         }
+        return matchingParticipants;
     }
 }
