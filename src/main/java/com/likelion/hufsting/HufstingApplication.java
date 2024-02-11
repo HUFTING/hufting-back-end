4c8c3c304b860b435bb85e55f9ef73be56c258c7
SeolHuiGwan9478 <shg5977@naver.com>
2024-02-12 04:48:23 +0900

    Feat: 알람 삭제 스케줄러 구현 완료

--- src/main/java/com/likelion/hufsting/HufstingApplication.java
+++ src/main/java/com/likelion/hufsting/HufstingApplication.java
@@ -4,11 +4,13 @@ package com.likelion.hufsting;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.TimeZone;
 
+@EnableScheduling
 @SpringBootApplication
 public class HufstingApplication {
 
--- src/main/java/com/likelion/hufsting/domain/matching/dto/matchingrequest/FindComeMatchingReqResponse.java
+++ src/main/java/com/likelion/hufsting/domain/matching/dto/matchingrequest/FindComeMatchingReqResponse.java
@@ -1,5 +1,6 @@
 package com.likelion.hufsting.domain.matching.dto.matchingrequest;
 
+import com.likelion.hufsting.domain.matching.domain.MatchingAcceptance;
 import com.likelion.hufsting.domain.matching.domain.MatchingStatus;
 import com.likelion.hufsting.global.dto.ResponseDto;
 import lombok.Builder;
@@ -12,7 +13,7 @@ import java.util.List;
 public class FindComeMatchingReqResponse implements ResponseDto {
     private Long matchingRequestId;
     private String matchingRequestTitle;
-    private MatchingStatus matchingStatus;
+    private MatchingAcceptance matchingAcceptance;
     private List<FindComeMatchingReqInParticipantData> participants;
     private List<FindComeMatchingReqInHostData> hosts;
 }
